7552fd0610e74c26cab08933f6c71b1b4a2ee254
Saga-sanga <vulcan248@gmail.com>
2023-05-16 17:04:54 +0530

    Add: Express server

--- nodeHTTPserver/app.js
+++ nodeHTTPserver/app.js
@@ -4,7 +4,22 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   console.log("We are in the Express App!!");
-  res.send("Hello World!");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+// redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+// The code is a catch all that executes for any parameter that reaches this point
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/views/404.html");
 });
 
 app.listen(port, () => {
